3eef7fddcb08794d72f9995d8ad02b092ba88bea
William Holmberg <williamholmberg@Williams-MacBook-Pro.local>
2019-09-13 12:38:01 +0200

    Updates

---
+++ precache-manifest.73c4bcb4c0c7bb2346f91d11f1ae394a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe37df4fca20b31c7908011c2fb3ac5",
+    "url": "/react-pages-test/index.html"
+  },
+  {
+    "revision": "9634cdd8b53ea0358d91",
+    "url": "/react-pages-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "37a0840e36c7f7e35b46",
+    "url": "/react-pages-test/static/js/2.e86806c9.chunk.js"
+  },
+  {
+    "revision": "9634cdd8b53ea0358d91",
+    "url": "/react-pages-test/static/js/main.aa333f65.chunk.js"
+  },
+  {
+    "revision": "729dc0b38e8faad249f9",
+    "url": "/react-pages-test/static/js/runtime~main.510e3748.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
