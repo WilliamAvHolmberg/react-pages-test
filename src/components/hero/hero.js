58873c6a1f8c6eea69bd9fb82619ba5ae1e2acd8
William Holmberg <william.av.holmberg@gmail.com>
2019-09-16 22:39:15 +0200

    Rework of front page with assistance of Malin Jakob

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import logo from '../../logo.svg';
 import './hero.scss'
 
 function Hero(){
+
+  const [siteLoaded, setLoaded] = useState(false);
+  useEffect(() => {
+    console.log("SITE INITIALIZED");
+    setLoaded(true);
+  }, []);
   return (
     <div className="hero">
-      <div className="hero-content">
-        <h1 className="hero-date"> 23 October</h1>
-        <h1 className="hero-desc"> Be part of the first <br/> King of Pong event! </h1>
+      <div className="hero-left-side">
+        <h1 className={'hero-date ' + (siteLoaded? 'show' : 'hidden') }> 23 October</h1>
+        <span className={'malin ' + (siteLoaded? 'show' : 'hidden')}></span>
+      </div>
+      <div className={'hero-right-side ' + (siteLoaded? 'show' : 'hidden')}>
+        <h1 className="hero-desc"> The first King of Pong event </h1>
         <div className="show-interest-button">
           <p>Sign up Now</p>
         </div>
