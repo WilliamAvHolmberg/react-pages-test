7733e26e2bfcb811f494db1ff521beb0619af575
William Holmberg <williamholmberg@Williams-MacBook-Pro.local>
2019-09-18 10:46:30 +0200

    Updates

---
+++ precache-manifest.5f4aa003ac3ac84ce65fd9ca49d48432.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964fbb0e434276e2cd18ffd54c7f7c5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7591f485021fcc472aa1",
+    "url": "/static/css/main.18feed72.chunk.css"
+  },
+  {
+    "revision": "bf242487b036eb9ba887",
+    "url": "/static/js/2.38a5a747.chunk.js"
+  },
+  {
+    "revision": "7591f485021fcc472aa1",
+    "url": "/static/js/main.93bb0688.chunk.js"
+  },
+  {
+    "revision": "335093769e51d0361e65",
+    "url": "/static/js/runtime~main.009b0cef.js"
+  }
+]);
