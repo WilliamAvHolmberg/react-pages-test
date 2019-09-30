b559da89a63b78eee63ab0f202ade4ee7e17fd15
Kristoffer Ahl <git@77dynamite.com>
2019-09-30 15:39:58 +0200

    - Footer and form fixes.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,15 +4,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhoneAlt, faEnvelope, faHome } from '@fortawesome/free-solid-svg-icons'
 
 function Footer () {
-    return (
-        <div className="footer-content">
-            <ul className="info-list">
-                <li><FontAwesomeIcon icon={faPhoneAlt} size="lg" style={{marginRight: '10px'}}/> +46 70 2969650</li>
-                <li><FontAwesomeIcon icon={faEnvelope} size="lg" style={{marginRight: '10px'}}/> <a href="mailto:info@dotnetmentor.se">info@dotnetmentor.se</a></li>
-                <li><FontAwesomeIcon icon={faHome}     size="lg" style={{marginRight: '10px'}}/> Första långgatan 22, 413 28 Göteborg</li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className='footer-content'>
+      <ul className='info-list'>
+        <li><h2>Get in touch!</h2></li>
+        <li><FontAwesomeIcon icon={faPhoneAlt} size='lg' style={{ marginRight: '10px' }} /> +46 70 2969650</li>
+        <li><FontAwesomeIcon icon={faEnvelope} size='lg' style={{ marginRight: '10px' }} /> <a href='mailto:info@dotnetmentor.se'>info@dotnetmentor.se</a></li>
+        <li><FontAwesomeIcon icon={faHome} size='lg' style={{ marginRight: '10px' }} /> Första långgatan 22, 413 28 Göteborg</li>
+      </ul>
+    </div>
+  )
 }
 
-export default Footer;
+export default Footer
--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,58 +1,56 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react'
 import './form.scss'
-function SignupForm(){
+function SignupForm () {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
 
-  const [name , setName] = useState('');
-  const [email , setEmail] = useState('');
-  const [phone, setPhone] = useState('');
+  const isEnabled = email.length > 0 && name.length > 0 && phone.length > 0
 
-  const isEnabled = email.length > 0 && name.length > 0 && phone.length > 0;
+  console.log(isEnabled)
 
-  console.log(isEnabled);
+  return (
+    <form action='https://formcarry.com/s/Ch1BYV2BzLV' method='POST' acceptCharset='UTF-8' >
 
-    return (
-      <form action="https://formcarry.com/s/Ch1BYV2BzLV" method="POST" acceptCharset="UTF-8" >
-
-
-      <input onChange={e => setName(e.target.value)} type="text" name="name" placeholder="Name"/>
-      <input onChange={e => setEmail(e.target.value)} type="email" name="email" placeholder="Email"/>
-      <input onChange={e => setPhone(e.target.value)} type="text" name="phone" placeholder="Phone"/>
-      {<div className="food-pref">
+      <input onChange={e => setName(e.target.value)} type='text' name='name' placeholder='Name' />
+      <input onChange={e => setEmail(e.target.value)} type='email' name='email' placeholder='Email' />
+      <input onChange={e => setPhone(e.target.value)} type='text' name='phone' placeholder='Phone' />
+      {<div className='food-pref'>
         <p>Questions? Food preferences? Leave us a message!</p>
-        <textarea rows={3} cols={40} name="message" placeholder="Gluten, vegetarian etc.."/>
-    </div>}
-      <div className="radio-wrapper">
-        <div className="radio-container">
-          <h2>Occupation:</h2>
-          <div className="radio-buttons">
-            <div className="radio-button">
-              <input type="radio" name="occupation" value="it-field" checked/><p>IT-Field</p>
+        <textarea rows={3} cols={40} name='message' placeholder='Gluten, vegetarian etc..' />
+      </div>}
+      <div className='radio-wrapper'>
+        <div className='radio-container'>
+          <div className='radio-buttons'>
+            <p>I'm:</p>
+            <div className='radio-button'>
+              <input type='radio' id='occupation-working' name='occupation' value='working' checked /><label for='occupation-working'>Working</label>
             </div>
-            <div className="radio-button">
-              <input type="radio" name="occupation" value="school"/><p>School</p>
+            <div className='radio-button'>
+              <input type='radio' id='occupation-school' name='occupation' value='school' /><label for='occupation-school'>In school</label>
             </div>
-            <div className="radio-button">
-              <input type="radio" name="occupation" value="other"/><p>Other</p>
+            <div className='radio-button'>
+              <input type='radio' id='occupation-other' name='occupation' value='other' /><label for='occupation-other'>Other</label>
             </div>
           </div>
         </div>
-        <div className="radio-container">
-          <h2>I want to:</h2>
-          <div className="radio-buttons">
-            <div className="radio-button">
-              <input type="radio" name="event_role" value="competitor" checked/><p>Compete</p>
+        <div className='radio-container'>
+          <div className='radio-buttons'>
+            <p>I want to:</p>
+            <div className='radio-button'>
+              <input type='radio' name='event_role' id='role-competitor' value='competitor' checked /><label for='role-competitor'>Compete</label>
             </div>
-            <div className="radio-button">
-              <input type="radio" name="event_role" value="attendee"/><p>Just attend</p>
+            <div className='radio-button'>
+              <input type='radio' name='event_role' id='role-attendee' value='attendee' /><label for='role-attendee'>Just attend</label>
             </div>
           </div>
         </div>
       </div>
-      <input onChange={e => setName(e.target.value)} type="hidden" name="_gotcha"/>
+      <input onChange={e => setName(e.target.value)} type='hidden' name='_gotcha' />
 
-      <button disabled={!isEnabled} className={(isEnabled? 'button enabled' : 'disabled')}> Submit </button>
+      <button disabled={!isEnabled} className={(isEnabled ? 'button enabled' : 'disabled')}> Submit </button>
     </form>
-    )
+  )
 }
 
-export default SignupForm;
+export default SignupForm
