252ff858cd2cc4a22eec255000a34dc307ad9ef2
Kristoffer Ahl <git@77dynamite.com>
2019-10-04 16:17:16 +0200

    - Changed contact email.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -9,7 +9,7 @@ function Footer () {
       <ul className='info-list'>
         <li><h2>Get in touch!</h2></li>
         <li><FontAwesomeIcon icon={faPhoneAlt} size='lg' style={{ marginRight: '10px' }} /> +46 70 2969650</li>
-        <li><FontAwesomeIcon icon={faEnvelope} size='lg' style={{ marginRight: '10px' }} /> <a href='mailto:info@dotnetmentor.se'>info@dotnetmentor.se</a></li>
+        <li><FontAwesomeIcon icon={faEnvelope} size='lg' style={{ marginRight: '10px' }} /> <a href='mailto:kingofpong@dotnetmentor.se'>kingofpong@dotnetmentor.se</a></li>
         <li><FontAwesomeIcon icon={faHome} size='lg' style={{ marginRight: '10px' }} /> Första långgatan 22, 413 28 Göteborg</li>
       </ul>
     </div>
