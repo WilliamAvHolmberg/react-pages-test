2000c19614548d9f52e0555662c43fdc7327b340
Kristoffer Ahl <git@77dynamite.com>
2019-10-04 16:41:04 +0200

    - Added FB event link.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,14 +2,15 @@ import React from 'react'
 import './footer.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhoneAlt, faEnvelope, faHome } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
 
 function Footer () {
   return (
     <div className='footer-content'>
       <ul className='info-list'>
-        <li><h2>Get in touch!</h2></li>
-        <li><FontAwesomeIcon icon={faPhoneAlt} size='lg' style={{ marginRight: '10px' }} /> +46 70 2969650</li>
+        <li><FontAwesomeIcon icon={faFacebookSquare} size='lg' style={{ marginRight: '10px' }} /> <a href='https://www.facebook.com/events/2427690890842280/'>Event page</a></li>
         <li><FontAwesomeIcon icon={faEnvelope} size='lg' style={{ marginRight: '10px' }} /> <a href='mailto:kingofpong@dotnetmentor.se'>kingofpong@dotnetmentor.se</a></li>
+        <li><FontAwesomeIcon icon={faPhoneAlt} size='lg' style={{ marginRight: '10px' }} /> +46 70 2969650</li>
         <li><FontAwesomeIcon icon={faHome} size='lg' style={{ marginRight: '10px' }} /> Första långgatan 22, 413 28 Göteborg</li>
       </ul>
     </div>
