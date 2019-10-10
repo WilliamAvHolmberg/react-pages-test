0124814da255fbb483616496954edaac08f03ee9
Kristoffer Ahl <git@77dynamite.com>
2019-10-10 18:28:07 +0200

    - Added facebook links to social section.

--- src/components/social-section/social-section.js
+++ src/components/social-section/social-section.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './social-section.scss'
 import ReadMoreButton from '../read-more-button/read-more-button'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
 
 function SocialSection () {
   return (
@@ -16,7 +18,15 @@ function SocialSection () {
           </div>
           <div className='info-row'>
             <p className='info-text'>
-                Enjoy a night of <strong>music</strong>, <strong>food</strong> and <strong>drinks</strong> while meeting new people and <strong>competing</strong> for the grand prize in our first ever tournament!
+              Enjoy a night of <strong>music</strong>, <strong>food</strong> and <strong>drinks</strong> while meeting new people and <strong>competing</strong> for the grand prize in our first ever tournament!
+            </p>
+          </div>
+          <div className='info-row'>
+            <p className='info-text'>
+              Check out our facebook event for news and updates.<br /><br />
+              <a href='https://www.facebook.com/events/2427690890842280/' target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faFacebookSquare} style={{ marginRight: '5px' }} /> event</a>
+              &nbsp;&nbsp;&nbsp;
+              <a href='http://fb.me/kingofpongsweden' target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faFacebookSquare} style={{ marginRight: '5px' }} /> page</a>
             </p>
           </div>
         </div>
